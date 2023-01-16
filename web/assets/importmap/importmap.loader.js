97fb55e7c512913c3016d1915a3fce2bc0119dd0
shc0743 <shc0743@outlook.com>
2023-01-16 12:38:41 +0800

    update importmap

--- web/assets/importmap/importmap.loader.js
+++ web/assets/importmap/importmap.loader.js
@@ -1,25 +1,23 @@
 (function (isProd) {
-    // copy from maps (External import maps are not yet supported.)
+  const maps = {
+    dev: 'assets/importmap/index.dev.importmap',
+    prod: 'assets/importmap/index.prod.importmap',
+  };
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', isProd ? maps.prod : maps.dev, false);
+  let content = '';
+  try {
+    xhr.send();
+    if (xhr.status >= 400) throw xhr;
+    content = (xhr.response);
+  }
+  catch (error) {
+    console.error('[importmap]', 'FATAL: Failed to get import map:', error);
+    return error;
+  }
 
-    const devMap = {
-        "imports": {
-            "vue": "./modules/vue/vue.esm-browser.js",
-            "idb": "./modules/idb/idb.esm.js",
-            "element-plus": "./modules/element-plus/index.full.min.mjs.js",
-            "element-plus/": "./modules/element-plus/"
-        }
-    };
-    const prodMap = {
-        "imports": {
-            "vue": "./modules/vue/vue.esm-browser.prod.js",
-            "idb": "./modules/idb/idb.esm.js",
-            "element-plus": "./modules/element-plus/index.full.min.mjs.js",
-            "element-plus/": "./modules/element-plus/"
-        }
-    };
-
-    const el = document.createElement('script');
-    el.type = 'importmap';
-    el.textContent = JSON.stringify(isProd ? prodMap : devMap);
-    (document.head || document.documentElement).append(el);
+  const el = document.createElement('script');
+  el.type = 'importmap';
+  el.textContent = JSON.stringify(content);
+  document.currentScript.append(el);
 })(false);
