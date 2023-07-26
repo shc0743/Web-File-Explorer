9db06477fa66e49f0858b7c64d53a7d651119cdd
shc0743 <shc0743@outlook.com>
2023-07-27 02:22:35 +0800

    [bug] 完善01fd8c1e03ea1e587a341cf5f1b624146298c1f5

--- web/assets/sw/env.js
+++ web/assets/sw/env.js
@@ -10,17 +10,33 @@ globalThis.sw_env = {
 };
 
 globalThis.sw_replaces = {
-    async 'assets/sw/sw_content.js'(req) {
-        if (!navigator.onLine) {
-            const cacheResult = await caches.match(req);
-            return cacheResult || fetch(req).catch(nop);
-        }
-        let pn = globalThis.location.pathname;
-        pn = pn.substring(0, pn.lastIndexOf('/'));
-        const resp = await fetch(pn + '/assets/sw/sw_content-bridge.js');
-        try { (await caches.open(sw_env.cacheName)).put(req, resp.clone()); } catch { }
-        return resp;
+    end: {
+        async 'assets/sw/sw_content.js'(req) {
+            if (!navigator.onLine) {
+                const cacheResult = await caches.match(req);
+                return cacheResult || fetch(req).catch(nop);
+            }
+            let pn = globalThis.location.pathname;
+            pn = pn.substring(0, pn.lastIndexOf('/'));
+            const resp = await fetch(pn + '/assets/sw/sw_content-bridge.js');
+            try { (await caches.open(sw_env.cacheName)).put(req, resp.clone()); } catch { }
+            return resp;
+        },
     },
+    start: {
+        '/file/new/': nativeFileApiFilter,
+    },
+    regexp: {
+        '^\/(dl|file|files|isFileOrDirectory|fileinfo)\?([\s\S]*)=([\s\S]*)$': nativeFileApiFilter,
+        '^\/(auth|volumes)$': nativeFileApiFilter,
+        '^\/file\/(copy|move|link)?([\s\S]*)=([\s\S]*)$': nativeFileApiFilter,
+        '^\/sys\/([A-z0-9]*)$': nativeFileApiFilter,
+    }
 };
+
+
+function nativeFileApiFilter(req) {
+    return fetch(req);
+}
 
 
--- web/sw.js
+++ web/sw.js
@@ -19,9 +19,15 @@ globalThis.addEventListener('activate', function (e) {
 globalThis.addEventListener('fetch', function (e) {
     e.respondWith(async function () {
         const url = String(e.request.url);
-        for (const i in globalThis.sw_replaces) {
-            if (url.endsWith(i)) return await globalThis.sw_replaces[i](e.request);
+        for (const i in globalThis.sw_replaces.end) {
+            if (url.endsWith(i)) return await globalThis.sw_replaces.end[i](e.request);
         }
+        for (const i in globalThis.sw_replaces.start) {
+            if (url.startsWith(globalThis.location.origin + i)) return await globalThis.sw_replaces.start[i](e.request);
+        }
+        for (const i in globalThis.sw_replaces.regexp) try {
+            if (new (RegExp)(i).test(url.replace(globalThis.location.origin, ''))) return await globalThis.sw_replaces.regexp[i](e.request);
+        } catch (error) { console.warn('[sw]', 'Failed to parse sw_replaces RegExp item:', error); }
         if (!navigator.onLine) {
             const cacheResult = await caches.match(e.request);
             return cacheResult || fetch(e.request).catch(nop);
@@ -29,8 +35,7 @@ globalThis.addEventListener('fetch', function (e) {
         try {
             const resp = await fetch(e.request);
             if (/GET/i.test(e.request.method) && resp.status === 200 && (
-                e.request.url.startsWith(globalThis.location.origin) &&
-                (!e.request.url.startsWith(globalThis.location.origin + '/dl?'))
+                e.request.url.startsWith(globalThis.location.origin)
             )) try {
                 const cache = await caches.open(cacheName);
                 cache.put(e.request, resp.clone());
